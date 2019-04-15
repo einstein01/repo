b015664787efc37b97270857fc6e2d6cb2940dfc
link <13207178516@163.com>
2019-04-15 19:42:34 +0800

    ssm_code2

---
+++ ssm_code02_parent/ssm_code02_service/src/main/java/com/itheima/service/impl/ItemsServiceImpl.java
@@ -0,0 +1,22 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.ItemsDao;
+import com.itheima.domain.Items;
+import com.itheima.service.ItemsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Davis
+ * @date 2019/4/14 19:31
+ */
+@Service
+public class ItemsServiceImpl implements ItemsService {
+
+    @Autowired
+    private ItemsDao itemsDao;
+
+    public Items findById(Integer id) {
+        return itemsDao.findById(id);
+    }
+}
