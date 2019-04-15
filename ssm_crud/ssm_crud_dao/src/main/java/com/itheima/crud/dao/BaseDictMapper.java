d590c378da0b00f822a75cac6942825e48e63913
link <13207178516@163.com>
2019-04-15 19:39:48 +0800

    ssm_code2测试git用例---------------------

---
+++ ssm_crud/ssm_crud_dao/src/main/java/com/itheima/crud/dao/BaseDictMapper.java
@@ -0,0 +1,10 @@
+package com.itheima.crud.dao;
+
+/**
+ * 字典数据表持久化接口
+ * @author Steven
+ *
+ */
+public interface BaseDictMapper {
+
+}
---
+++ ssm_crud/ssm_crud_dao/src/main/java/com/itheima/crud/dao/CustomerMapper.java
@@ -0,0 +1,10 @@
+package com.itheima.crud.dao;
+
+/**
+ * 客户信息持久化接口
+ * @author Steven
+ *
+ */
+public interface CustomerMapper {
+
+}
